c6856ac0a26b6c5562fbe91f6e22a33110fab1d8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-07-19 12:56:07 +0600

    one-to-one

--- main.py
+++ main.py
@@ -18,10 +18,20 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("author", sqlalchemy.String),
+    sqlalchemy.Column("reader_id", sqlalchemy.ForeignKey("readers.id"), nullable=False, index=True),
 )
 
-engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all(engine)
+readers = sqlalchemy.Table(
+    "readers",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("first_name", sqlalchemy.String),
+    sqlalchemy.Column("last_name", sqlalchemy.String),
+)
+
+
+# engine = sqlalchemy.create_engine(DATABASE_URL)
+# metadata.create_all(engine)
 
 app = FastAPI()
 
@@ -46,5 +56,13 @@ async def read_books():
 async def create_book(request: Request):
     data = await request.json()
     query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
+
+
+@app.post("/readers/")
+async def create_reader(request: Request):
+    data = await request.json()
+    query = readers.insert().values(**data)
     last_record_id = await database.execute(query)
     return {"id": last_record_id}
