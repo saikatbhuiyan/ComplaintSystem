40ec20591d6a595b8c89bcbfde0f589b2e0db46c
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-07-19 16:02:23 +0600

    Enum

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import enum
 import databases
 import sqlalchemy
 from decouple import config
@@ -36,6 +37,59 @@ readers_books = sqlalchemy.Table(
     sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
     sqlalchemy. Column("book_id", sqlalchemy.ForeignKey("books.id"), nullable=False, index=True),
     sqlalchemy.Column("reader_id", sqlalchemy.ForeignKey("readers.id"), nullable=False, index=True),
+)
+
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String(120), unique=True),
+    sqlalchemy.Column("password", sqlalchemy.String(255)),
+    sqlalchemy.Column("full_name", sqlalchemy.String(200)),
+    sqlalchemy.Column("phone", sqlalchemy.String(13)),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
+    sqlalchemy.Column(
+        "last_modified_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
+)
+
+class ColorEnum(enum.Enum):
+    pink = "pink"
+    black = "black"
+    white = "white"
+    yellow = "yellow"
+
+
+class SizeEnum(enum.Enum):
+    xs = "xs"
+    s = "s"
+    m = "m"
+    l = "l"
+    xl = "xl"
+    xxl = "xxl"
+
+
+clothes = sqlalchemy.Table(
+    "clothes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String(120)),
+    sqlalchemy.Column("color", sqlalchemy.Enum(ColorEnum), nullable=False),
+    sqlalchemy.Column("size", sqlalchemy.Enum(SizeEnum), nullable=False),
+    sqlalchemy.Column("photo_url", sqlalchemy.String(255)),
+    sqlalchemy.Column("created_at", sqlalchemy.DateTime, nullable=False, server_default=sqlalchemy.func.now()),
+    sqlalchemy.Column(
+        "last_modified_at",
+        sqlalchemy.DateTime,
+        nullable=False,
+        server_default=sqlalchemy.func.now(),
+        onupdate=sqlalchemy.func.now(),
+    ),
 )
 
 
